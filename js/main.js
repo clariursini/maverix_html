2f0a41c417c24192f480e0a34c5490e4ad193574
Clari Ursini <clari_ursini@hotmail.com>
2022-10-28 03:06:52 -0300

    added nav bar effect

--- js/main.js
+++ js/main.js
@@ -1,22 +1,16 @@
+// ------------------- NAV BAR MOBILE -------------------//
+
+/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
+function hamburger() {
+  var x = document.getElementById("nav-link");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+
 // ------------------- STICKY NAV BAR -------------------//
-
-// When the user scrolls the page, execute myFunction
-// window.onscroll = function() {stickyNavbar()};
-
-// Get the navbar
-// var navbar = document.getElementById("navbar");
-
-// Get the offset position of the navbar
-// var sticky = navbar.offsetTop;
-
-// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-// function stickyNavbar() {
-//   if (window.pageYOffset >= sticky) {
-//     navbar.classList.add("sticky")
-//   } else {
-//     navbar.classList.remove("sticky");
-//   }
-// }
 
 document.addEventListener("scroll", function () {
   const navbar = document.querySelector(".navbar");
